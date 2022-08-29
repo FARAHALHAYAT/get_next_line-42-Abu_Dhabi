7982cee22757c8dba499efbb3cdd984f2e48fb1c
fasharif <farahshraif@gmail.com>
2022-08-29 19:01:29 +0400

    Create get_next_line_bonus.h

---
+++ get_next_line_bonus.h
@@ -0,0 +1,15 @@
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*ft_read_and_save(int fd, char *save);
+char	*ft_save(char *save);
+char	*ft_get_line(char *save);
+size_t	ft_strlen(char *str);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+
+#endif
