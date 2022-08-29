15aa8d2aa844d1cc70d0c40582c28f16494ebfc3
fasharif <farahshraif@gmail.com>
2022-08-29 19:06:00 +0400

    Create main_bonus.c

---
+++ main_bonus.c
@@ -0,0 +1,35 @@
+#include "get_next_line_bonus.h"
+#include <stdio.h>
+#include <fcntl.h>
+
+int	main(void)
+{
+	int		fd;
+	int		fd2;
+	char	*line;
+	char	*line2;
+
+	fd = open("test.txt", O_RDONLY);
+	fd2 = open("test2.txt", O_RDONLY);
+	printf("fd: %d\n", fd);
+	printf("fd2: %d\n", fd2);
+
+	line = get_next_line(fd);
+	printf("%s", line);
+	line2 = get_next_line(fd2);
+	printf("%s", line2);
+
+	free(line);
+	free(line2);
+
+	line = get_next_line(fd);
+	printf("%s", line);
+	line2 = get_next_line(fd2);
+	printf("%s", line2);
+
+	free(line);
+	free(line2);
+	close(fd);
+	close(fd2);
+	return (0);
+}
