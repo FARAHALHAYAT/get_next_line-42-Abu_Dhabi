771dd1d0573ddc8c0d823ad2525d32d2476b9591
fasharif <farahshraif@gmail.com>
2022-09-03 00:31:02 +0400

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -24,7 +24,7 @@ size_t	ft_strlen(char *str)
 	return (l);
 }
 
-char	*ft_strchr(char *s, int c)
+char	*ft_strchr(char *s, int c) // Function to check if there is a matching character in a string If the character exists, return a pointer to where the character exists; otherwise, a null pointer is returned.
 {
 	int	i;
 
@@ -48,7 +48,7 @@ char	*ft_strjoin(char *s1, char *s2)
 	size_t	c;
 	char	*str;
 
-	if (!s1)
+	if (!s1) //Handle when s1 is an empty string
 	{
 		s1 = (char *)malloc(1 * sizeof(char));
 		s1[0] = '\0';
